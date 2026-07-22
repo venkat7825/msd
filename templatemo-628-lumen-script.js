2079d5a021bd4f519fefdceeeac2beb96463d28e
venkat7825 <sv4236434@gmail.com>
2026-07-22 13:24:33 +0530

    Add files via upload

---
+++ templatemo-628-lumen-script.js
@@ -0,0 +1,26 @@
+body{
+    font-family: Arial;
+    margin:0;
+    background:#f4f4f4;
+}
+
+header{
+    background:#0066cc;
+    color:white;
+    padding:40px;
+    text-align:center;
+}
+
+.box{
+    background:white;
+    padding:20px;
+    margin:20px;
+    border-radius:10px;
+}
+
+footer{
+    background:#222;
+    color:white;
+    text-align:center;
+    padding:15px;
+}
